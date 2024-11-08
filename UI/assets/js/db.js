75d9ec863016f560514d25147db9af3efd1cb031
KingCreatBug <trunghoang.240500@gmail.com>
2024-11-08 16:43:18 +0700

    add: about, book, doctor, medical, specialty page

--- UI/assets/js/db.js
+++ UI/assets/js/db.js
@@ -104,5 +104,92 @@ export const data = {
             specialized: "Gây mê hồi xuân",
             part: "Khoa Gây mê Phẫu thuật - Phòng khám Đa khoa Fresh-X",
         },
+        {
+            avatar: "/UI/assets/img/doctor/avt1.png",
+            name: "Hoàng Bảo Trung",
+            specialty: "Bác sĩ chuyên khoa II",
+            specialized: "Gây mê hồi xuân",
+            part: "Khoa Gây mê Phẫu thuật - Phòng khám Đa khoa Fresh-X",
+        },
+    ],
+
+    speci: [
+        {
+            image: "/UI/assets/img/speci/item1.png",
+            title: "Tim mạch",
+            desc: "Tại các phòng khám trên khắp hệ thốngFresh-X, Khoa Tim mạch được đầu tư mạnh và đây là một trong những chuyên khoa chủ lực của chúng tôi. Các bác sĩ và đội ngũ nhân viên khám chữa bệnh được đào tạo chuyên sâu, với bằng cấp tại các cơ sở đào tạo",
+            explain:
+                "Tại các phòng khám trên khắp hệ thống Fresh-X, Khoa Tim mạch được đầu tư mạnh và đây là một trong những chuyên khoa chủ lực của chúng tôi. Các bác sĩ và đội ngũ nhân viên khám chữa bệnh được đào tạo chuyên sâu, với bằng cấp tại các cơ sở đào tạo y khoa danh tiếng Việt Nam. Ngoài ra, trang thiết bị hiện đại, tiên tiến cũng giúp nâng cao hiệu quả chẩn đoán và điều trị. Một loạt các ca phẫu thuật tim mạch phức tạp mà trước đây chỉ có thể tiến hành ở nước ngoài thì nay đã được thực hiện ngay ở Việt Nam: tại các phòng khám Fresh-X. ",
+        },
+        {
+            image: "/UI/assets/img/speci/item2.png",
+            title: "Da liễu",
+            desc: "Tại các phòng khám trong hệ thống Fresh-X, Khoa Da liễu được đầu tư đồng bộ, là một trong những chuyên khoa quan trọng của chúng tôi. Đội ngũ bác sĩ và nhân viên y tế của Khoa Da liễu được đào tạo bài bản và chuyên sâu, sở hữu bằng cấp từ các cơ sở đào tạo uy tín, giúp đảm bảo chất lượng khám và điều trị cho bệnh nhân. ",
+            explain:
+                'Khoa Da liễu tại Fresh-X cam kết mang đến dịch vụ chăm sóc toàn diện cho làn da của bạn và gia đình. Chúng tôi hiểu rằng "Làn da khỏe mạnh là nền tảng của sự tự tin", vì vậy đội ngũ bác sĩ của chúng tôi luôn tận tâm đồng hành cùng bạn trong hành trình chăm sóc và cải thiện làn da. Khoa Da liễu được trang bị các thiết bị hiện đại, từ máy laser trị liệu, máy soi da kỹ thuật số, đến công nghệ trẻ hóa và tái tạo da, giúp bạn đạt được kết quả như mong muốn. Ngoài ra, chúng tôi còn có các dịch vụ hỗ trợ tại Khoa Thẩm mỹ, đáp ứng mọi nhu cầu từ điều trị da chuyên sâu đến chăm sóc da hàng ngày. Với sự hợp tác cùng các chuyên gia đầu ngành, Hoàn Mỹ tự hào mang đến dịch vụ chăm sóc da chất lượng, chu đáo và chi phí hợp lý, luôn đặt sức khỏe và sự hài lòng của bạn lên hàng đầu.',
+        },
+        {
+            image: "/UI/assets/img/speci/item3.png",
+            title: "Nhi khoa",
+            desc: "Nhi khoa cung cấp các dịch vụ như khám, chẩn đoán, tư vấn và điều trị nội trú, ngoại trú cho trẻ em.",
+            explain:
+                "Nhi khoa cung cấp các dịch vụ như khám, chẩn đoán, tư vấn và điều trị nội trú, ngoại trú cho trẻ em.",
+        },
+        {
+            image: "/UI/assets/img/speci/item4.png",
+            title: "Chấn thương chỉnh hình",
+            desc: "Khoa Chỉnh hình của chúng tôi có nhiệm vụ khám chữa các rối loạn về xương, khớp và hệ thống cơ xương ở cả trẻ em và người lớn. Chúng tôi tin rằng “Sống là phải vận động”. Khoa Chỉnh hình của chúng tôi được trang bị đầy đủ, hiện đại với đội ngũ chuyên",
+            explain:
+                'Khoa Chỉnh hình của chúng tôi có nhiệm vụ khám chữa các rối loạn về xương, khớp và hệ thống cơ xương ở cả trẻ em và người lớn. Chúng tôi tin rằng "Sống là phải vận động". Khoa Chỉnh hình của chúng tôi được trang bị đầy đủ, hiện đại với đội ngũ chuyên gia luôn sẵn sàng giúp bạn duy trì cuộc sống khỏe mạnh, năng động. Các bệnh viện của chúng tôi có trang thiết bị đầu ngành (bao gồm máy quét MRI, máy quét MSCT 64 lát cắt, máy chụp C-Arm, máy nội soi khớp và thiết bị trị liệu thần kinh cột sống) cùng với các dịch vụ chăm sóc chuyên sâu tại Khoa Vật lý trị liệu và Phục hồi chức năng. Fresh-X cộng tác với các bác sĩ chuyên khoa hàng đầu về Chỉnh hình để không ngừng mang lại cho người dân Việt Nam dịch vụ chăm sóc sức khỏe bền vững, lấy bệnh nhân làm trung tâm với chi phí hợp lý.',
+        },
+        {
+            image: "/UI/assets/img/speci/item5.png",
+            title: "Thần kinh",
+            desc: "Tại hệ thống Fresh-X, Khoa Thần kinh được chú trọng đầu tư với trang thiết bị hiện đại và phương pháp điều trị tiên tiến. Đội ngũ bác sĩ chuyên khoa giàu kinh nghiệm, được đào tạo bài bản, tận tâm chăm sóc và mang đến giải pháp điều trị hiệu quả cho các vấn đề về thần kinh, giúp bệnh nhân an tâm điều trị và phục hồi tốt nhất. ",
+            explain:
+                'Khoa Thần kinh tại Fresh-X cam kết chăm sóc sức khỏe não bộ và hệ thần kinh của bạn với sự tận tâm và chuyên môn cao. Chúng tôi hiểu rằng "Sức khỏe thần kinh là nền tảng của cuộc sống khỏe mạnh", vì vậy đội ngũ bác sĩ và chuyên gia giàu kinh nghiệm của chúng tôi luôn đồng hành cùng bạn trong việc phòng ngừa, chẩn đoán và điều trị các vấn đề về thần kinh. Khoa Thần kinh được trang bị hệ thống thiết bị hiện đại, bao gồm máy MRI, máy CT đa lát cắt, máy điện não đồ và các công nghệ tiên tiến khác, giúp hỗ trợ chẩn đoán chính xác và điều trị hiệu quả. Chúng tôi cũng cung cấp các dịch vụ hỗ trợ chuyên sâu tại Khoa Vật lý trị liệu và Phục hồi chức năng, đảm bảo mang lại cho bạn giải pháp điều trị toàn diện và bền vững. Hoàn Mỹ tự hào mang đến dịch vụ chăm sóc thần kinh chất lượng, với chi phí hợp lý, luôn lấy sức khỏe và sự hài lòng của bạn làm trọng tâm.',
+        },
+    ],
+
+    maternity: [
+        {
+            image: "/UI/assets/img/medical/maternity1.png",
+            link: "",
+            title: "Sinh mổ",
+            desc: "Gói trọn an tâm khi chọn Sinh mổ tại phòng khám Fresh-X. Mẹ và bé sẽ nhận được hỗ trợ chăm sóc y tế toàn diện từ trước, trong và sau sinh. Tất cả gói trọn trong gói dịch vụ này.",
+        },
+        {
+            image: "/UI/assets/img/medical/maternity2.png",
+            link: "",
+            title: "Sinh thường",
+            desc: "Gói trọn an tâm khi Sinh thường tại phòng khám Fresh-X. Mẹ và bé sẽ nhận được hỗ trợ chăm sóc y tế toàn diện từ trước, trong và sau sinh. Tất cả gói trọn trong gói dịch vụ này.",
+        },
+        {
+            image: "/UI/assets/img/medical/maternity3.png",
+            link: "",
+            title: "Sinh con trọn gói",
+            desc: "Gói trọn an tâm khi chọn Sinh con trọn Fresh-X. Mẹ và bé sẽ nhận được hỗ trợ chăm sóc y tế toàn diện từ trước, trong và sau sinh. Tất cả gói trọn trong gói dịch vụ này.",
+        },
+    ],
+
+    physical: [
+        {
+            image: "/UI/assets/img/medical/physical1.png",
+            link: "",
+            title: "Khám sức khỏe gia đình",
+            desc: "Gói khám gia đình giúp bạn chủ động bảo vệ và phòng ngừa các bệnh lý nguy cơ từ sớm cho những người thân yêu. Từ đó giúp gia đình bạn điều trị kịp thời và tối ưu hoá chi phí. Đầu tư vào sức khoẻ dài hạn ngay hôm nay.",
+        },
+        {
+            image: "/UI/assets/img/medical/physical2.png",
+            link: "",
+            title: "Khám sức khỏe doanh nghiệp",
+            desc: "Nâng cao sức khỏe doanh nghiệp với dịch vụ Kiểm tra sức khỏe doanh nghiệp ngay hôm nay. Chúng tôi đặc biệt cung cấp các gói kiểm tra linh hoạt, có thể thực hiện tại văn phòng của bạn hoặc tại bệnh viện. Minh mẩn và khoẻ mạnh là chìa khoá vàng",
+        },
+        {
+            image: "/UI/assets/img/medical/physical3.png",
+            link: "",
+            title: "Khám sức khỏe tổng quát",
+            desc: "Gói khám mang đến cho bạn sự an tâm với sự kỹ lưỡng trong khám, xét nghiệm, đánh và tư vấn chuyên môn từ đội ngũ y tế lành nghề; từ đó giúp phát hiện và điều trị kịp thời bệnh lý nguy hiểm.",
+        },
     ],
 };
